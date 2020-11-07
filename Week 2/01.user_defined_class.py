898b545e67085e9d54dd0512452c32e1db9aabe8
JOY <32105827+nhjoy@users.noreply.github.com>
2020-11-07 16:12:04 +0800

    constructor added

---
+++ Week 2/01.user_defined_class.py
@@ -0,0 +1,5 @@
+class Point():                  #convention class name starts with capital letter
+    def __init__(self, x, y):   #contructor to set instances
+        self.x = 0
+        self.y = 0
+    def 
