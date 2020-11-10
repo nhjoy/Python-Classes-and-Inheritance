a647f66af2638f910a47c7b548ff29ecc4f926e7
JOY <32105827+nhjoy@users.noreply.github.com>
2020-11-10 18:50:51 +0800

    class_bike problem solved

---
+++ Assignment 1/class_bike.py
@@ -0,0 +1,15 @@
+"""
+Define a class called Bike that accepts a string and a float as input, 
+and assigns those inputs respectively to two instance variables, color 
+and price. Assign to the variable testOne an instance of Bike whose color 
+is blue and whose price is 89.99. Assign to the variable testTwo an 
+instance of Bike whose color is purple and whose price is 25.0.
+"""
+class Bike:
+
+    def __init__(self, paint, value):
+        self.color = paint
+        self.price = value
+
+testOne = Bike("blue", 89.99)
+testTwo = Bike("purple", 25.0)
